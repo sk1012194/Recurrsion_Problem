fc7263320ab1f6c21bdd7b6d449c0f5127e53629
sk1012194 <127843333+sk1012194@users.noreply.github.com>
2023-12-05 02:12:02 +0530

    Add files via upload

---
+++ maximumcuttingofrod.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<limits.h>
+int solve(int n,int x,int y,int z){
+    if(n==0){
+        return 0;
+    }
+    if(n<0){
+        return INT16_MIN;
+    }
+    int ans1=solve(n-x,x,y,z)+1;
+    int ans2=solve(n-y,x,y,z)+1;
+    int ans3=solve(n-z,x,y,z)+1;
+    int ans=max(ans1,max(ans2,ans3));
+    return ans;
+}
+int main(){
+    int n=7;
+    int x=5;
+    int y=2;
+    int z=2;
+    int ans=solve(n,x,y,z);
+    cout<<ans;
+}
